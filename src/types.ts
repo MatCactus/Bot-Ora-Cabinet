8e17ed64c37e5893b325db8c4238b83b2cf3e750
MatCactus <lepluart.matteo@gmail.com>
2022-04-23 11:43:52 +0200

    Setup & Clean

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,1 @@
-import { Message } from "discord.js";
-
-export interface dbFile {
-    guilds: { name: string; guildId: string; commands?: string[] }[]; // All Auth Guilds Id with there attributed commands
-    vote: {
-        interactionId: string;
-        message: Message;
-        authorId: string;
-        votes: { userId: string; vote: "negative" | "positive" }[];
-    }[];
-}
+export interface dbFile {}
