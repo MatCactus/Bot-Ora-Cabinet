b0a2bce9161b43da9da839ad9399fee540ad692d
MatCactus <lepluart.matteo@gmail.com>
2022-04-24 14:46:28 +0200

    Update of Data Types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
-export interface dbFile {}
+export interface dbFile {
+    dm: {
+        clientId: string;
+        channelId: string;
+        messageId: string;
+    }[]; // clientId: Discord Id of the Sender | channelId: Discord Id of which channel is assigned to him | messageId: Reply Message Id (Embed)
+}
